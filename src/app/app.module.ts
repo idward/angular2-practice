f2ff3a206854b7809fa75d0b9c99198ba0a6aa16
idwardluo <apple@idwardluo.local>
2017-07-10 07:59:43 +0800

    update route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
+import {RouterModule} from "@angular/router";
 // import {CommonModule} from "@angular/common";
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {EmployeeComponent} from './employee/employee.component';
+import {DepartmentComponent} from './department/department.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpModule,
+    RouterModule.forRoot([
+      {path: 'department', component: DepartmentComponent},
+      {path: 'employee', component: EmployeeComponent}
+    ])
     //CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
