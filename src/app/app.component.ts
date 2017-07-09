ee57fff5d8d9ee4f0c5a6c247d2e7d4dd0537f82
idwardluo <apple@idwardluo.local>
2017-07-09 21:47:26 +0800

    update angular2-practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,38 @@
-import {Component} from '@angular/core';
-import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // color:string = 'blue';
   // one:boolean = true;
   // two:boolean = true;
 
   myForm:FormGroup;
 
-  constructor() {
-    this.myForm = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(4)]),
-      email: new FormControl(),
-      address: new FormGroup({
-        street: new FormControl(),
-        city: new FormControl(),
-        postalCode: new FormControl()
+  constructor(private _formBuilder:FormBuilder) {
+    // this.myForm = new FormGroup({
+    //   name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    //   email: new FormControl(),
+    //   address: new FormGroup({
+    //     street: new FormControl(),
+    //     city: new FormControl(),
+    //     postalCode: new FormControl()
+    //   })
+    // });
+  }
+
+  ngOnInit():any {
+    this.myForm = this._formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      email: [],
+      address: this._formBuilder.group({
+        street: [],
+        city: [],
+        postalCode: []
       })
     });
   }
