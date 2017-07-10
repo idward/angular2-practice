173f19134c9743972f14b8e7b76b867089774417
idwardluo <apple@idwardluo.local>
2017-07-10 15:06:03 +0800

    update route

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
+
 import {DepartmentComponent} from "../department/department.component";
 import {EmployeeComponent} from "../employee/employee.component";
 import {DepartmentDetailComponent} from "../department/department-detail/department-detail.component";
+import {HomeComponent} from "../home/home.component";
+import {PageNoFoundComponent} from "../page-no-found/page-no-found.component";
 
 const routes:Routes = [
+  // {path: '', component: HomeComponent},
+  {path: '', redirectTo: '/department', pathMatch: 'full'},
   {path: 'department', component: DepartmentComponent},
   {path: 'employee', component: EmployeeComponent},
-  {path: 'department/:id', component: DepartmentDetailComponent}
+  {path: 'department/:id', component: DepartmentDetailComponent},
+  {path: '**', component: PageNoFoundComponent}
 ];
 
 @NgModule({
@@ -16,8 +22,9 @@ const routes:Routes = [
     CommonModule,
     RouterModule.forRoot(routes)
   ],
-  exports:[RouterModule],
-  declarations: [DepartmentComponent,EmployeeComponent,DepartmentDetailComponent]
+  exports: [RouterModule],
+  declarations: [HomeComponent, PageNoFoundComponent, DepartmentComponent, EmployeeComponent, DepartmentDetailComponent]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
 
